e8df269c7cb033e65e2173b3e816d2734728911b
seagull <24163551@qq.com>
2017-11-15 17:31:56 +0800

    update .gitignore

---
+++ mysqldb/update(alter table)/V2.3 upgrade V2.4/V2.4.sql
@@ -0,0 +1,4 @@
+/*调度表增加客户端load驱动桩路径字段*/
+alter table TEST_JOBS add clientpath VARCHAR(100) COMMENT '客户端测试驱动桩路径';
+/*调度表增加客户端load驱动桩路径字段*/
+alter table TEST_CLIENT add clientpath VARCHAR(100) COMMENT '客户端测试驱动桩路径 多个;做分隔';
