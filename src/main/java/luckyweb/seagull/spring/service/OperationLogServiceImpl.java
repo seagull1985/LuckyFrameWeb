e405cb16ab5a99ada0ea4e66318c3d40670dd1d0
leo <1009518044@qq.com>
2018-10-23 14:31:59 +0800

    test

--- src/main/java/luckyweb/seagull/spring/service/OperationLogServiceImpl.java
+++ src/main/java/luckyweb/seagull/spring/service/OperationLogServiceImpl.java
@@ -38,7 +38,6 @@ public class OperationLogServiceImpl implements OperationLogService{
 	
 	@Override
 	public OperationLog load(int id) throws Exception {
-		// TODO Auto-generated method stub		
 		return this.operationlogdao.load(id);
 	}
 	
