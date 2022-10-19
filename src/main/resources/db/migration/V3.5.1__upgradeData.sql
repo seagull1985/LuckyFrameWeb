670f4e1a192318218d2b3f02da0201e2f220eb6b
chenzaixing <chenzaixing@ysstech.com>
2022-10-19 14:28:19 +0800

    fix:PO-sql脚本升级报错

--- src/main/resources/db/migration/V3.5.1__upgradeData.sql
+++ src/main/resources/db/migration/V3.5.1__upgradeData.sql
@@ -7,7 +7,9 @@
  ************  WARNING  ************   
  此脚本属于应用自动升级数据库表结构以及数据脚本，无需手动执行，请慎重！！！！
 */
---1、PO模式功能
+-- ----------------------------
+-- 页面配置
+-- ----------------------------
 CREATE TABLE if  not exists  `project_page_object`  (
                                         `project_id` int NULL DEFAULT NULL COMMENT '关联项目ID',
                                         `create_by` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '创建者',
@@ -23,6 +25,9 @@ CREATE TABLE if  not exists  `project_page_object`  (
                                         PRIMARY KEY (`page_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 29 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '页面配置管理' ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- 页面元素关联表
+-- ----------------------------
 CREATE TABLE if  not exists  `project_page_detail`  (
                                         `id` int NOT NULL AUTO_INCREMENT COMMENT '系统主键',
                                         `create_by` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '创建者',
@@ -37,13 +42,13 @@ CREATE TABLE if  not exists  `project_page_detail`  (
                                         PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 76 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '页面详情' ROW_FORMAT = DYNAMIC;
 
-INSERT INTO sys_menu (`menu_id`, menu_name`, parent_id`, order_num`, url`, menu_type`, visible`, perms`, icon`, create_by`, create_time`, update_by`, update_time`, remark`) VALUES (2001, '页面管理', 4, 3, '/testmanagmt/projectPageObject', 'C', '0', 'testmanagmt:projectPageObject:view', '#', 'admin', '2018-03-01 00:00:00', 'dyl', '2022-03-31 17:48:28', '页面配置管理菜单');
-INSERT INTO sys_menu (`menu_id`, menu_name`, parent_id`, order_num`, url`, menu_type`, visible`, perms`, icon`, create_by`, create_time`, update_by`, update_time`, remark`) VALUES (2002, '页面配置管理查询', 2001, 1, '#', 'F', '0', 'testmanagmt:projectPageObject:list', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:28:36', '');
-INSERT INTO sys_menu (`menu_id`, menu_name`, parent_id`, order_num`, url`, menu_type`, visible`, perms`, icon`, create_by`, create_time`, update_by`, update_time`, remark`) VALUES (2003, '页面配置管理新增', 2001, 2, '#', 'F', '0', 'testmanagmt:projectPageObject:add', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:28:52', '');
-INSERT INTO sys_menu (`menu_id`, menu_name`, parent_id`, order_num`, url`, menu_type`, visible`, perms`, icon`, create_by`, create_time`, update_by`, update_time`, remark`) VALUES (2004, '页面配置管理修改', 2001, 3, '#', 'F', '0', 'testmanagmt:projectPageObject:edit', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:29:00', '');
-INSERT INTO sys_menu (`menu_id`, menu_name`, parent_id`, order_num`, url`, menu_type`, visible`, perms`, icon`, create_by`, create_time`, update_by`, update_time`, remark`) VALUES (2005, '页面配置管理删除', 2001, 4, '#', 'F', '0', 'testmanagmt:projectPageObject:remove', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:29:08', '');
-INSERT INTO sys_menu (`menu_id`, menu_name`, parent_id`, order_num`, url`, menu_type`, visible`, perms`, icon`, create_by`, create_time`, update_by`, update_time`, remark`) VALUES (2006, '页面详情', 4, 1, '/testmanagmt/projectPageDetail', 'C', '1', 'testmanagmt:projectPageDetail:view', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-03-10 17:32:07', '页面详情菜单');
-INSERT INTO sys_menu (`menu_id`, menu_name`, parent_id`, order_num`, url`, menu_type`, visible`, perms`, icon`, create_by`, create_time`, update_by`, update_time`, remark`) VALUES (2007, '页面详情查询', 2006, 1, '#', 'F', '0', 'testmanagmt:projectPageDetail:list', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:28:24', '');
-INSERT INTO sys_menu (`menu_id`, menu_name`, parent_id`, order_num`, url`, menu_type`, visible`, perms`, icon`, create_by`, create_time`, update_by`, update_time`, remark`) VALUES (2008, '页面详情新增', 2006, 2, '#', 'F', '0', 'testmanagmt:projectPageDetail:add', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:29:47', '');
-INSERT INTO sys_menu (`menu_id`, menu_name`, parent_id`, order_num`, url`, menu_type`, visible`, perms`, icon`, create_by`, create_time`, update_by`, update_time`, remark`) VALUES (2009, '页面详情修改', 2006, 3, '#', 'F', '0', 'testmanagmt:projectPageDetail:edit', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:29:53', '');
-INSERT INTO sys_menu (`menu_id`, menu_name`, parent_id`, order_num`, url`, menu_type`, visible`, perms`, icon`, create_by`, create_time`, update_by`, update_time`, remark`) VALUES (2010, '页面详情删除', 2006, 4, '#', 'F', '0', 'testmanagmt:projectPageDetail:remove', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:30:00', '');
+INSERT INTO sys_menu (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `menu_type`, `visible`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (2001, '页面管理', 4, 3, '/testmanagmt/projectPageObject', 'C', '0', 'testmanagmt:projectPageObject:view', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-03-31 17:48:28', '页面配置管理菜单');
+INSERT INTO sys_menu (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `menu_type`, `visible`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (2002, '页面配置管理查询', 2001, 1, '#', 'F', '0', 'testmanagmt:projectPageObject:list', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:28:36', '');
+INSERT INTO sys_menu (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `menu_type`, `visible`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (2003, '页面配置管理新增', 2001, 2, '#', 'F', '0', 'testmanagmt:projectPageObject:add', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:28:52', '');
+INSERT INTO sys_menu (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `menu_type`, `visible`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (2004, '页面配置管理修改', 2001, 3, '#', 'F', '0', 'testmanagmt:projectPageObject:edit', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:29:00', '');
+INSERT INTO sys_menu (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `menu_type`, `visible`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (2005, '页面配置管理删除', 2001, 4, '#', 'F', '0', 'testmanagmt:projectPageObject:remove', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:29:08', '');
+INSERT INTO sys_menu (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `menu_type`, `visible`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (2006, '页面详情', 4, 1, '/testmanagmt/projectPageDetail', 'C', '1', 'testmanagmt:projectPageDetail:view', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-03-10 17:32:07', '页面详情菜单');
+INSERT INTO sys_menu (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `menu_type`, `visible`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (2007, '页面详情查询', 2006, 1, '#', 'F', '0', 'testmanagmt:projectPageDetail:list', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:28:24', '');
+INSERT INTO sys_menu (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `menu_type`, `visible`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (2008, '页面详情新增', 2006, 2, '#', 'F', '0', 'testmanagmt:projectPageDetail:add', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:29:47', '');
+INSERT INTO sys_menu (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `menu_type`, `visible`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (2009, '页面详情修改', 2006, 3, '#', 'F', '0', 'testmanagmt:projectPageDetail:edit', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:29:53', '');
+INSERT INTO sys_menu (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `menu_type`, `visible`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (2010, '页面详情删除', 2006, 4, '#', 'F', '0', 'testmanagmt:projectPageDetail:remove', '#', 'admin', '2018-03-01 00:00:00', 'admin', '2022-04-01 11:30:00', '');
