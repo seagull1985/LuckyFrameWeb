87b83e8101990b8908b48d63a327caf4437d8d41
seagull <heyanlin@qq.com>
2023-03-23 16:27:27 +0800

    单词描述错误

--- src/main/resources/static/ajax/libs/bootstrap-table/extensions/editable/bootstrap-editable.js
+++ src/main/resources/static/ajax/libs/bootstrap-table/extensions/editable/bootstrap-editable.js
@@ -531,7 +531,7 @@ Editableform is linked with one of input types, e.g. 'text', 'select' etc.
         Success callback. Called when value successfully sent on server and **response status = 200**.  
         Usefull to work with json response. For example, if your backend response can be <code>{success: true}</code>
         or `{success: false, msg: "server error"}` you can check it inside this callback.  
-        If it returns **string** - means error occured and string is shown as error message.  
+        If it returns **string** - means error occurred and string is shown as error message.
         If it returns **object like** `{newValue: &lt;something&gt;}` - it overwrites value, submitted by user
         (useful when server changes value).  
         Otherwise newValue simply rendered into element.
